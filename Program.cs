a3b2252f189f32c87d81c5a5574e3d11e9ac2721
Vian Gosani <howrain1@gmail.com>
2019-04-02 12:09:45 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hasil;
+            Calculator cal = new Calculator();
+            hasil = cal.Penjumlahan(10, 2);
+            CetakHasil("Hasil Penjumlahan = " + hasil);
+            hasil = cal.Penjumlahan(10, 2, 3);
+            CetakHasil("Hasil Penjumlahan overload = " + hasil);
+            hasil = cal.Pengurangan(7, 2);
+            CetakHasil("Hasil Pengurangan = " + hasil);
+            hasil = Calculator.Perkalian(5, 2);
+            CetakHasil("Hasil Perkalian = " + hasil);
+            Console.ReadKey();
+        }
+        static void CetakHasil(string hasil)
+        {
+            Console.WriteLine(hasil);
+        }
+    }
+}
